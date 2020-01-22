4743536de4f9b3777860668e05d714de9f7524be
Nedzib Sastoque <nedzib.sastoque@gmail.com>
2020-01-22 12:38:13 -0500

    local bootstrap added

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -2,7 +2,7 @@ import { Cliente } from './cliente';
 
 export const CLIENTES: Cliente[]=[
     {id: 1, nombre: 'Ned', apellido: 'Sas', email: 'nedsas@gmail.com', fecha: '2020-01-21'},
-    {id: 2, nombre: 'Lus', apellido: 'Tab', email: 'lustab@gmail.com', fecha: '2010-02-12'},
+    {id: 2, nombre: 'Lui', apellido: 'Tab', email: 'lustab@gmail.com', fecha: '2010-02-12'},
     {id: 3, nombre: 'Sur', apellido: 'Mal', email: 'surmal@gmail.com', fecha: '2020-03-29'},
     {id: 4, nombre: 'Dum', apellido: 'Raf', email: 'dumraf@gmail.com', fecha: '2020-06-31'},
     {id: 5, nombre: 'And', apellido: 'Fed', email: 'andfed@gmail.com', fecha: '2020-02-21'},
