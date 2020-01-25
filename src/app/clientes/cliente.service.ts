8cb82ce8a3319ad07629a1ce0b08e79a57768984
Brayan Nedzib Sastoque Rangel <nedzib.sastoque@gmail.com>
2020-01-25 07:52:37 -0500

    add the http client 25102020-752

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe( //Castear el contenido de urlEndPoint para poder devolverlo como un arreglo de objetos cliente
+      map(response => response as Cliente[]) 
+    );
   }
-}
+ }
